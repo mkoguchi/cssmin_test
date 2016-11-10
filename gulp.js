76ae672adda2a265175036f3f1a72e456af89fd7
小口 征孝 <mkoguchi@crooz.co.jp>
2016-11-10 20:21:31 +0900

    超とちゅう

---
+++ gulp.js
@@ -0,0 +1,7 @@
+var gulp = require('gulp');
+var cssmin = require('gulp-cssmin');
+var sass = require('gulp-sass');
+var sourcemaps = require('gulp-sourcemaps');
+var browsersync = require('browser-sync');
+
+
